3ff68ca7a4879d0f471b1d5c408f4c59127abf10
ericzhangle <ericzhangle@gmail.com>
2017-10-06 23:43:54 +0800

    Initial Commit

---
+++ FileManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FileManager
+//
+//  Created by tomzhangle on 10/6/17.
+//  Copyright © 2017 tomzhangle. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FileManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FileManager
+//
+//  Created by tomzhangle on 10/6/17.
+//  Copyright © 2017 tomzhangle. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
